6c2ba1c9d69cc66f9c21e92255958d42d700e13d
Musa Minnakhmetov <gtr425gtr@gmail.com>
2020-05-12 02:37:43 +0300

    Added creations Quiz, rounds, setting for time between questions

--- handlers/new_quiz.py
+++ handlers/new_quiz.py
@@ -7,17 +7,42 @@ from quizzer import Quiz, Question, quiz, current_round
 
 
 class CreateQuiz(StatesGroup):
-    waiting_for_game_name = State()
+    waiting_for_quiz_name = State()
     waiting_for_round_name = State()
     waiting_for_time_between_questions = State()
+    waiting_for_question = State()
     waiting_for_quiz = State()
 
-@dp.message_handler(state=CreateQuiz.waiting_for_game_name)
-async def set_name_for_quiz(message: types.Message):
-    game = Quiz(message.text) # Подставьте сюда свой Telegram ID
-    await CreateQuiz.waiting_for_round_name.set()
-    await message.reply("Пришлите название Вашего раунда (например, «История математики»).", reply_markup=types.ReplyKeyboardRemove())
 
-@dp.message_handler(state=CreateQuiz.waiting_for_round_name)
+@dp.message_handler(state=CreateQuiz.waiting_for_quiz_name, content_types=types.ContentTypes.TEXT)
+async def set_name_for_quiz(message: types.Message):
+    global quiz
+    quiz = Quiz(message.text) # Подставьте сюда свой Telegram ID
+    await CreateQuiz.waiting_for_round_name.set()
+    await message.reply(
+        'Пришлите название Вашего раунда (например, «История математики»).',
+        reply_markup=types.ReplyKeyboardRemove()
+    )
+
+
+@dp.message_handler(state=CreateQuiz.waiting_for_round_name, content_types=types.ContentTypes.TEXT)
 async def set_name_for_round(message: types.Message):
+    global quiz
+    quiz.add_round(message.text)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('15 сек.', '30 сек.', '1 мин.', '5 мин.', '10 мин.', '15 мин.')
+    await CreateQuiz.waiting_for_time_between_questions.set()
+    await message.reply('Какой промежуток будет между вопросами?', reply_markup=keyboard)
+
+
+@dp.message_handler(state=CreateQuiz.waiting_for_time_between_questions, content_types=types.ContentTypes.TEXT)
+async def set_time_between_questions(message: types.Message):
+    global quiz
+    quiz.set_time_between_quiestions(message.text)
+    poll_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    poll_keyboard.add(types.KeyboardButton(text="Создать вопрос",
+                                           request_poll=types.KeyboardButtonPollType(type=types.PollType.QUIZ)))
+    await CreateQuiz.waiting_for_question.set()
+    await message.answer("Создайте вопрос, для этого используйте кнопку ниже.",
+                         reply_markup=poll_keyboard)
 
